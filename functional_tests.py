4e8bc97a61b82db0e834b4d8aa6379e090d0c577
rickdeu <hangaloandre@gmail.com>
2022-10-19 23:54:25 +0100

    Basic view now returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,7 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
     def tearDown(self):
-        self.browser.quite()
+        self.browser.quit()
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
